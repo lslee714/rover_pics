6250523f834a2917800056a4a8590df83c52c02e
Luke Lee <lslee714@gmail.com>
2019-12-03 19:43:27 -0700

    Make an app parent component, restructure

---
+++ app/views/home/components/rover-pics.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Nav from './nav';
+import { RoverSelect } from './rover-select';
+import { Row, Col } from 'react-bootstrap';
+
+class App extends React.Component {
+    render() {
+        return (
+        <>
+        <Nav/>
+        <Row>
+            <Col>
+                <RoverSelect/>
+            </Col>
+            <Col>
+                <h1>Hi</h1>
+            </Col>
+        </Row>
+        </>
+        )
+    }
+}
+
+export default App;
--- app/views/home/index.js
+++ app/views/home/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Nav from './components/nav';
-import RoverSelect  from './components/rover-select';
-
 import '../stylesheets/home/home.scss';
+import App from './components/rover-pics';
 
-ReactDOM.render(<Nav />, document.getElementById('navbar'));
-ReactDOM.render(<RoverSelect />, document.getElementById('rovers'));
+
+ReactDOM.render(<App />, document.getElementById('app'));
