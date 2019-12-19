d90482e7a4ec4000404814e1949960640ca6d2dc
Luke Lee <lslee714@gmail.com>
2019-12-18 18:30:27 -0700

    Use the port specified in the docker container to allow connection

--- app/server.js
+++ app/server.js
@@ -1,13 +1,13 @@
 const path = require('path');
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 8081;
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '/views'))
-app.use(express.static('app/dist'))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static('app/dist'));
 
 require('./routes/home.js')(app); 
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
